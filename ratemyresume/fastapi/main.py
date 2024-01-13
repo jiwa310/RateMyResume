c5511466238465ea53f8386a19bb4ad11576eba9
Jim Wang <jimwang@ucsb.edu>
2024-01-13 13:14:45 -0800

    fastapi

--- ratemyresume/fastapi/main.py
+++ ratemyresume/fastapi/main.py
@@ -4,13 +4,24 @@ from io import BytesIO
 
 app = FastAPI()
 
+# Initialize a variable to store the number of pages
+num_pages = None
+
+@app.get("/")
+async def read_root():
+    # Check if a PDF has been uploaded
+    if num_pages is not None:
+        return {"Number of pages in last uploaded PDF": num_pages}
+    else:
+        return {"message": "No PDF uploaded yet"}
+
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
+    global num_pages
     contents = await file.read()
-    reader = PyPDF2.PdfFileReader(BytesIO(contents))
+    reader = PyPDF2.PdfReader(BytesIO(contents))
 
-    #USE READER TO DO WHATEVER YOU WANT
-    #using reader to get the number of pages in the pdf:
-    print(reader.getNumPages())
+    # Store the number of pages in the global variable
+    num_pages = len(reader.pages)
 
-    return {"filename": file.filename}
+    return {"filename": file.filename, "num_pages": num_pages}
